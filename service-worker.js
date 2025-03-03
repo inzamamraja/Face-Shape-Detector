efe5c30f9e59c8f3d0029a85b45133d855169cc5
Inzamam Raja <42240068+inzamamraja@users.noreply.github.com>
2025-03-03 18:44:24 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,7 @@
 self.addEventListener("install", (event) => {
+    self.skipWaiting();
+});
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("pwa-cache").then((cache) => {
       return cache.addAll([
