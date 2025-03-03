921ca3f2c38d5bf1a5e17fa3a07d308c6eff2a94
Inzamam Raja <42240068+inzamamraja@users.noreply.github.com>
2025-03-03 18:40:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 navigator.serviceWorker.ready.then(swRegistration => {
   return swRegistration.sync.register("sync-data");
+  if ("serviceWorker" in navigator && "SyncManager" in window) {
+    navigator.serviceWorker.ready.then(registration => {
+        return registration.sync.register("sync-data");
+    }).catch(err => console.error("Background sync failed:", err));
+}
 });
